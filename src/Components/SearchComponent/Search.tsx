4bfe7a857724c4459c7e092fc6a4336b61225645
Joel De Zoysa <joel.de.zoysa@gmail.com>
2020-07-26 01:26:50 +1200

    Add search button

--- src/Components/SearchComponent/Search.tsx
+++ src/Components/SearchComponent/Search.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Grid, TextField } from '@material-ui/core';
+import { Button, Grid, TextField } from '@material-ui/core';
 import './Search.css';
 import { IUserInput } from '../../Common/Interfaces';
 
@@ -17,7 +17,16 @@ function Search(props: ISearchProps) {
 
     const [HasFocus, setHasFocus] = useState<boolean>(false);
 
-    
+    const handleSubmit = () => {
+        if (SearchQuery?.length !== 0 && SearchQuery !== null && SearchQuery !== "") {
+            let UserInput: IUserInput = {
+                SearchQuery: SearchQuery,
+            }
+            props.SetUserInput(UserInput);
+        } else {
+            setHasFocus(true);
+        }
+    }
 
     return <div className="SearchContainer">
         <Grid container spacing={3}>
@@ -25,13 +34,16 @@ function Search(props: ISearchProps) {
                 <TextField
                     required
                     id="outlined-required"
-                    label="Search"
+                    label="Image URL"
                     variant="outlined"
                     error={HasFocus && SearchQuery === ""}
                     onClick={() => setHasFocus(true)}
                     value={SearchQuery}
                     onChange={e => handleSearchQueryChange(e.target.value)}
                 />
+                <Button variant="contained" color="primary" onClick={handleSubmit}>
+                    Submit
+                </Button>
             </Grid>
         </Grid>
     </div>
